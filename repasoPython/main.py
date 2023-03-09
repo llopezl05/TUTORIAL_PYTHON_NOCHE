1c63dc609a022e4f44ca8e8c0021755ce1c9c3ce
llopezl05 <leidijohanalopez@gmail.com>
2023-03-09 10:47:42 -0500

    se agregan los tipos de datos

---
+++ repasoPython/main.py
@@ -0,0 +1,26 @@
+#tipos de datos PRIMITIVOS
+#NUMEROS ENTEROS
+numero1: int = 78
+numero2 = 48
+
+print(numero1)
+print(numero2)
+
+#NUMEROS REALES
+numero3: float = 4.9
+numero4 = 5.0
+
+print(numero3)
+print(numero4)
+
+#NUMERO BOOLEANOS toman dos valores verdadero o falso
+
+esColombiano: bool = True
+esArgentino = False
+
+print(esColombiano)
+print(esArgentino)
+
+#CARACTER Y CADENA DE CARACTERES
+mensaje = "cadena con una comilla simple ', una comilla doble \" y una diagonal invertida \\"
+print(mensaje)
